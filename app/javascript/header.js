ca5b6769ded08d2aae6fac6f96851ba137f7cd72
tokiya-takai <t.orca.dolphin@gmail.com>
2021-01-26 17:05:49 +0900

    ヘッダー機能の実装

--- app/javascript/header.js
+++ app/javascript/header.js
@@ -1,23 +1,26 @@
 function userButton() {
-  const leftButton = document.getElementById("left-btn");
-  const rightButton = document.getElementById("right-btn");
+  // const leftButton = document.getElementById("left-btn");
+  // const rightButton = document.getElementById("right-btn");
+  const signInButton = document.querySelector('.sign_in_button');
+  const signUpButton = document.querySelector('.sign_up_button');
 
-  leftButton.addEventListener('mouseover', () => {
-    leftButton.style.backgroundColor = "#7c6060";
+  signInButton.addEventListener('mouseover', () => {
+    signInButton.style.borderBottom = "2px solid #2d2020";
   });
-  leftButton.addEventListener('mouseout', () => {
-    leftButton.style.backgroundColor = "#5a4040";
+  signInButton.addEventListener('mouseout', () => {
+    signInButton.style.borderBottom = "none";
   });
 
-  rightButton.addEventListener('mouseover', () => {
-    rightButton.style.backgroundColor = "#4f4040";
+  signUpButton.addEventListener('mouseover', () => {
+    signUpButton.style.borderBottom = "2px solid #2d2020";
   });
-  rightButton.addEventListener('mouseout', () => {
-    rightButton.style.backgroundColor = "#2d2020";
+  signUpButton.addEventListener('mouseout', () => {
+    signUpButton.style.borderBottom = "none";
   });
 }
 
 function findList() {
+
   const find = document.getElementById("find");
   const findList = document.getElementById("find-list");
   const findListZone = document.getElementById("find-list-zone")
@@ -49,10 +52,24 @@ function navChoiceUnderBar() {
     });
     navItem.addEventListener('mouseout', () => {
       navItem.classList.remove('nav_item_border');
-    })
-  })
+    });
+  });
+}
+
+function listItemOpacity() {
+  const listItems = document.querySelectorAll('.list_item');
+
+  listItems.forEach(function (listItem) {
+    listItem.addEventListener('mouseover', () => {
+      listItem.style.opacity = 0.7;
+    });
+    listItem.addEventListener('mouseout', () => {
+      listItem.style.opacity = 1;
+    });
+  });
 }
 
 window.addEventListener('load', userButton);
 window.addEventListener('load', findList);
 window.addEventListener('load', navChoiceUnderBar);
+window.addEventListener('load', listItemOpacity);
