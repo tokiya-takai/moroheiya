d20277630ed3477cff56a3889fac9eb9a70424f6
tokiya-takai <t.orca.dolphin@gmail.com>
2021-01-30 17:33:07 +0900

    バグの修正

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,9 @@ module Moroheiya
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # config.i18n.default_locale = :ja
+    # config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.yml').to_s]
+
     # バリデーションエラーの際にレイアウトが崩れるのを防ぐ
     config.action_view.field_error_proc = Proc.new do |html_tag, instance| 
       html_tag
