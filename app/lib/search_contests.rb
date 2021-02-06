b6d0ecd69b73f2ef9d271a586fa78bcdd13172c0
tokiya-takai <t.orca.dolphin@gmail.com>
2021-02-06 15:46:08 +0900

    検索機能と表示機能のバグの修正

--- app/lib/search_contests.rb
+++ app/lib/search_contests.rb
@@ -1,7 +1,7 @@
 class SearchContests
   def self.search(search)
     if search != ""
-      Contest.where('title like ?',"%#{search}%")
+      Contest.where('title like ? OR content like ?',"%#{search}%","%#{search}%")
     else
       Contest.all
     end
