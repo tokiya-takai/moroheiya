60b40625ead1ec8e9ff304b634404ff872256f28
tokiya-takai <t.orca.dolphin@gmail.com>
2021-02-08 17:17:36 +0900

    エラー文を追加

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,6 +8,17 @@ class OrdersController < ApplicationController
   end
 
   def confirm
+    @order = Order.new(order_params)
     binding.pry
+    if (@order.black == nil) && (@order.gold == nil) && (@order.purple == nil)
+      @order.errors[:base] << "1つ以上選択してください。"
+      render :new
+    end
+  end
+
+
+  private
+  def order_params
+    params.require(:order).permit(:black,:gold,:purple).merge(user_id: current_user.id)
   end
 end
