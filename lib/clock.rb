1d442edf3be2aacf2f8f57e5cd455ad1807927b1
tokiya-takai <t.orca.dolphin@gmail.com>
2021-02-15 18:39:28 +0900

    clockworkを導入し、1時間に1回記録

--- lib/clock.rb
+++ lib/clock.rb
@@ -1,19 +1,24 @@
 require 'clockwork'
 include Clockwork
 
-every(1.hour, 'deadline') do
+require File.expand_path('../../config/boot', __FILE__)
+require File.expand_path('../../config/environment', __FILE__)
+
+every(1.hour, 'deadline', at: '**:00') do
   contests = Contest.all
   contests.each do | contest |
     if is_end?(contest)
       contest.end = true
       contest.save
+      file = File.open('clockwork_log.txt', 'a')
+      file.puts(DateTime.now)
     end
   end
 end
 
 def is_end?(contest)
   now = DateTime.now
-  if now >= deadline
+  if now >= contest.deadline
     return true
   else
     return false
