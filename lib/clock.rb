b2b7ea640bbff1165152040597f5a021b3f5bf5c
tokiya-takai <t.orca.dolphin@gmail.com>
2021-02-15 18:46:32 +0900

    clockwork修正と23:59分実行の追加

--- lib/clock.rb
+++ lib/clock.rb
@@ -4,14 +4,23 @@ include Clockwork
 require File.expand_path('../../config/boot', __FILE__)
 require File.expand_path('../../config/environment', __FILE__)
 
-every(1.hour, 'deadline', at: '**:00') do
+every(1.hour, 'deadline1', at: '**:00') do
+  deadline
+end
+
+every(1.day, 'deadline2', at: '23:59') do
+  deadline
+end
+
+def deadline
   contests = Contest.all
   contests.each do | contest |
     if is_end?(contest)
       contest.end = true
       contest.save
       file = File.open('clockwork_log.txt', 'a')
-      file.puts(DateTime.now)
+      log = DateTime.now
+      file.puts log
     end
   end
 end
